9b370150ee10b59141be3185ce4504ae44f21922
Justin Andhika <75529515+justin-jac@users.noreply.github.com>
2023-06-09 13:29:46 +0700

    update

---
+++ staff_service/staffSQL/staff.sql
@@ -0,0 +1,70 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 09, 2023 at 08:24 AM
+-- Server version: 10.4.20-MariaDB
+-- PHP Version: 8.0.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `staf`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `stafs`
+--
+
+CREATE TABLE `stafs` (
+  `id` int(11) NOT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `nama` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `stafs`
+--
+
+INSERT INTO `stafs` (`id`, `email`, `nama`, `password`) VALUES
+(1, 'staf111@gmail.com', 'Marine', 'mariminum'),
+(2, 'staf222@gmail.com', 'Kumala', 'maritidur'),
+(3, 'staf333@gmail.com', 'Kanata', 'marimakan');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `stafs`
+--
+ALTER TABLE `stafs`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `stafs`
+--
+ALTER TABLE `stafs`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
