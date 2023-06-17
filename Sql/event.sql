fdbb9eb2c6de7a8beedc5f8f05058f4aa847f929
Justin Andhika <75529515+justin-jac@users.noreply.github.com>
2023-06-17 23:17:45 +0700

    Update event.sql

--- Sql/event.sql
+++ Sql/event.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 14, 2023 at 01:03 PM
--- Server version: 10.4.20-MariaDB
--- PHP Version: 8.0.9
+-- Generation Time: Jun 17, 2023 at 06:16 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -36,19 +36,56 @@ CREATE TABLE `events` (
   `event_name` varchar(255) DEFAULT NULL,
   `event_description` text DEFAULT NULL,
   `sub_total` int(11) DEFAULT NULL,
-  `event_date` date DEFAULT NULL,
   `start_time` time DEFAULT NULL,
   `end_time` time DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `events`
 --
 
-INSERT INTO `events` (`id_event`, `id_order`, `id_staffPIC`, `event_name`, `event_description`, `sub_total`, `event_date`, `start_time`, `end_time`) VALUES
-(1, 1, 1, 'Birthday', 'Sweet 17th so make it more merrier', 50, '2023-06-15', '09:00:00', '12:00:00'),
-(2, 2, 2, 'Marriage', 'Mr.Johnny Sins and Mrs.Winda is the groom and bride', 100, '2023-06-16', '14:00:00', '18:00:00'),
-(3, 3, 3, 'Birthday', 'Marine 10th birthday', 75, '2023-06-17', '10:00:00', '16:00:00');
+INSERT INTO `events` (`id_event`, `id_order`, `id_staffPIC`, `event_name`, `event_description`, `sub_total`, `start_time`, `end_time`) VALUES
+(1, 1, 1, 'Birthday', 'Sweet 17th so make it more merrier', 50, '09:00:00', '12:00:00'),
+(2, 1, 2, 'Marriage', 'Mr.Johnny Sins and Mrs.Winda is the groom and bride', 100, '14:00:00', '18:00:00'),
+(3, 2, 1, 'Birthday', 'Marine 10th birthday', 75, '10:00:00', '16:00:00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `event_order`
+--
+
+CREATE TABLE `event_order` (
+  `id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `event_order`
+--
+
+INSERT INTO `event_order` (`id`) VALUES
+(1),
+(2),
+(3);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `event_staff`
+--
+
+CREATE TABLE `event_staff` (
+  `id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `event_staff`
+--
+
+INSERT INTO `event_staff` (`id`) VALUES
+(1),
+(2),
+(3);
 
 --
 -- Indexes for dumped tables
@@ -59,6 +96,18 @@ INSERT INTO `events` (`id_event`, `id_order`, `id_staffPIC`, `event_name`, `even
 --
 ALTER TABLE `events`
   ADD PRIMARY KEY (`id_event`);
+
+--
+-- Indexes for table `event_order`
+--
+ALTER TABLE `event_order`
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- Indexes for table `event_staff`
+--
+ALTER TABLE `event_staff`
+  ADD UNIQUE KEY `id` (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
